3e21b77507d07389e81a0b306e18b13f4b59cd61
Jiageng <skyleaworlder@outlook.com>
2024-03-13 23:12:15 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+
+public static Apcomplex
+pow(Apcomplex z, long n) {
+  ...
+}
+
+
+float real_value = 1.0f;
+float imag_value = 2.0f;
+Apfloat real = new Apfloat(real_value);
+Apfloat imag = new Apfloat(imag_value);
+Apcomplex c1 = new Apcomplex(real, imag);
+
+
